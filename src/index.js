52b30dc95db753114c934eb3163ede14b736c42e
nikunjj1175 <nikunjkorat117@gmail.com>
2024-01-12 15:46:51 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants"
+// import express from "express";
+// const app = express();
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 9000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+
+
+
+// (async () =>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error", (error)=>{
+//             console.log("ERROR: ",error);
+//             throw error;
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+//     } catch (error){
+//         console.error("Error: ",error);
+//         throw error;
+//     }
+// })()
